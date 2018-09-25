6beccf47c0b3d99fcef73c5d0d15d21c6afbfb23
Felipe Pereira <felipeepereiras@gmail.com>
2018-09-24 22:00:36 -0300

    Criando as migration do sistema

---
+++ database/migrations/2018_09_25_005438_create_orders_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('descricao');
+            $table->string('data_pedido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
